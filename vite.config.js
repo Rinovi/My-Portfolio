fbd6e2521e7d5025642ba7dc29a4be24c4bf042e
Caleb Warford <cjwarford98@gmail.com>
2024-08-02 01:19:46 -0700

    Define port as 3000 in viteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    server: {
+        port: 3000,
+    },
     build: {
         rollupOptions: {
             input: {
