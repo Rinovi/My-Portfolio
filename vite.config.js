e69daa0b17f7dc48b0d96b5cb3ebbda2a9c9cc10
Caleb Warford <cjwarford98@gmail.com>
2024-08-02 01:35:01 -0700

    tried to update port declaration to fix issue with render deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-// vite.config.js
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
     server: {
-        port: 3000,
+        port: process.env.PORT || 3000, // Use the PORT environment variable if available, or default to 3000
     },
     build: {
         rollupOptions: {
